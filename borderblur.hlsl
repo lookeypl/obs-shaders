379354cb707c8732b76bb7fabf6ff7a3535104c4
Lookey <costyrra.xl@gmail.com>
2021-11-05 15:57:08 +0100

    Update borderblur.hlsl to include cropping

--- borderblur.hlsl
+++ borderblur.hlsl
@@ -2,14 +2,18 @@ uniform int Blur_left = 20;
 uniform int Blur_right = 20;
 uniform int Blur_top = 20;
 uniform int Blur_bottom = 20;
+uniform int Crop_left = 0;
+uniform int Crop_right = 0;
+uniform int Crop_top = 0;
+uniform int Crop_bottom = 0;
 
 float blurAscending(float from, float to, float current)
 {
     if (current > to)
         return 1.0f;
 
-    float a = -1.0f / (from - to);
-    float b = 1.0f - (to * a);
+    const float a = -1.0f / (from - to);
+    const float b = 1.0f - (to * a);
     float y = a * current + b;
     if (y < 0.0f)
         y = 0.0f;
@@ -23,8 +27,8 @@ float blurDescending(float from, float to, float current)
     if (current < from)
         return 1.0f;
 
-    float a = -1.0f / (to - from);
-    float b = 1.0f - (from * a);
+    const float a = -1.0f / (to - from);
+    const float b = 1.0f - (from * a);
     float y = a * current + b;
     if (y < 0.0f)
         y = 0.0f;
@@ -37,22 +41,26 @@ float4 mainImage(VertData v_in) : TARGET
 {
     float4 color = image.Sample(textureSampler, v_in.uv);
 
-    float blurLeft_uv_from = 0.0f;
-    float blurLeft_uv_to = (float)Blur_left * uv_pixel_interval.x;
+    const float blurLeft_uv_from = Crop_left * uv_pixel_interval.x;
+    const float blurRight_uv_to = 1.0f - (Crop_right * uv_pixel_interval.x);
+    const float blurTop_uv_from = Crop_top * uv_pixel_interval.y;
+    const float blurBottom_uv_to = 1.0f - (Crop_bottom * uv_pixel_interval.y);
+
+    if (v_in.uv.x < blurLeft_uv_from || v_in.uv.x > blurRight_uv_to ||
+        v_in.uv.y < blurTop_uv_from || v_in.uv.y > blurBottom_uv_to)
+        return float4(color.x, color.y, color.z, 0.0f);
+
+    float blurLeft_uv_to = blurLeft_uv_from + ((float)Blur_left * uv_pixel_interval.x);
     color.w = color.w * blurAscending(blurLeft_uv_from, blurLeft_uv_to, v_in.uv.x);
 
-    float blurRight_uv_from = 1.0f - ((float)Blur_right * uv_pixel_interval.x);
-    float blurRight_uv_to = 1.0f;
+    float blurRight_uv_from = blurRight_uv_to - ((float)Blur_right * uv_pixel_interval.x);
     color.w = color.w * blurDescending(blurRight_uv_from, blurRight_uv_to, v_in.uv.x);
 
-    float blurTop_uv_from = 0.0f;
     float blurTop_uv_to = (float)Blur_top * uv_pixel_interval.y;
     color.w = color.w * blurAscending(blurTop_uv_from, blurTop_uv_to, v_in.uv.y);
 
-    float blurBottom_uv_from = 1.0f - ((float)Blur_bottom * uv_pixel_interval.y);
-    float blurBottom_uv_to = 1.0f;
+    float blurBottom_uv_from = blurBottom_uv_to - ((float)Blur_bottom * uv_pixel_interval.y);
     color.w = color.w * blurDescending(blurBottom_uv_from, blurBottom_uv_to, v_in.uv.y);
-
 
     return color;
 }

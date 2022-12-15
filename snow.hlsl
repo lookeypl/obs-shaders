2df87823adb76795e544a1d85291e70c6e495a3d
Lookey <costyrra.xl@gmail.com>
2022-12-15 12:09:06 +0100

    snow: Modify code to allow some parametrization
    Adds parameters for "zoom-in" onto a scene and snowfall speed.
    
    Snow density was renamed to "snow iterations" to mark what the parameter
    actually does.
    
    Actual snow density (way less resource intenstive than previously) was
    added.

--- snow.hlsl
+++ snow.hlsl
@@ -6,8 +6,34 @@
  * Based on https://www.shadertoy.com/view/Mdt3Df
  */
 
-uniform int Snow_density = 12;
-uniform int Snow_layers = 2;
+#pragma shaderfilter set Snow_iterations__min 1
+#pragma shaderfilter set Snow_iterations__description "Snow iterations"
+uniform int Snow_iterations = 12; // affect how many snow flakes are rendered - GPU-EXPENSIVE
+
+#pragma shaderfilter set Snow_layers__min 1
+#pragma shaderfilter set Snow_layers__description "Snow layers"
+uniform int Snow_layers = 2; // multiply/layer Snow_loops - GPU-EXPENSIVE
+
+#pragma shaderfilter set Snow_density__slider true
+#pragma shaderfilter set Snow_density__description "Snow density"
+#pragma shaderfilter set Snow_density__min 0.08
+#pragma shaderfilter set Snow_density__max 0.6
+#pragma shaderfilter set Snow_density__step 0.04
+uniform float Snow_density = 0.08f; // affect how dense the snow is
+
+#pragma shaderfilter set Fall_speed__slider true
+#pragma shaderfilter set Fall_speed__description "Snow fall speed"
+#pragma shaderfilter set Fall_speed__min 0.3
+#pragma shaderfilter set Fall_speed__max 1.0
+#pragma shaderfilter set Fall_speed__step 0.05
+uniform float Fall_speed = 0.3f; // affect how fast the snow flakes fall
+
+#pragma shaderfilter set Scene_zoom__slider true
+#pragma shaderfilter set Scene_zoom__description "Scene zoom"
+#pragma shaderfilter set Scene_zoom__min 1.0
+#pragma shaderfilter set Scene_zoom__max 5.0
+#pragma shaderfilter set Scene_zoom__step 0.05
+uniform float Scene_zoom = 2.0f; // affect how zoomed in the scene is
 
 float rand(float2 uv_in)
 {
@@ -21,14 +47,14 @@ float4 render(float2 uv_in)
 
     for (int k = 0; k < Snow_layers; k++)
     {
-        for (int i = 0; i < Snow_density; i++)
+        for (int i = 0; i < Snow_iterations; i++)
         {
-            float cellSize = 2.0f + (i * 3.0f);
-            float downSpeed = -0.3f - (sin(builtin_elapsed_time * 0.4f + float(k + i * 20)) + 1.0f) * 0.00008f;
+            float cellSize = 3.0f + i / (Scene_zoom - 0.9);
+            float downSpeed = -Fall_speed - (sin(builtin_elapsed_time * 0.4f + float(k + i * 20)) + 0.6f) * 0.00008f;
 
             float2 uv = uv_in + float2(
-                0.01f * sin((builtin_elapsed_time + float(k * 6185)) * 0.6 + float(i)) * (5.0f / float(i)),
-                downSpeed * (builtin_elapsed_time + float(k * 1352)) * (1.0 / float(i))
+                0.02f * sin((builtin_elapsed_time + float(k * 6185)) * 0.4f + i) * (i * 0.2f),
+                downSpeed * (builtin_elapsed_time + float(k * 3128)) * (1.0f / float(i))
             );
             float2 uvStep = ceil(uv * cellSize - 0.5f) / cellSize;
             float x = frac(sin(dot(uvStep, float2(12.9898 + float(k) * 12.0, 78.233 + float(k) * 315.156))) * 43758.5453 + float(k) * 12.0) - 0.5;
@@ -40,13 +66,13 @@ float4 render(float2 uv_in)
             float d = 5.0f * distance((uvStep + float2(x * sin(y), y) * randomMagnitude1 + float2(y, x) * randomMagnitude2), uv);
 
             float omiVal = frac(sin(dot(uvStep, float2(32.4691, 94.615))) * 31572.1684);
-            if (omiVal < 0.08)
+            if (omiVal < Snow_density)
             {
-                float newd = (x + 1) * 0.4f * clamp(1.9f - d * (15.0f + (x * 6.3f)) * (cellSize / 1.4f), 0.0f, 1.0f);
+                float newd = (x + 1) * 0.4f * clamp(1.9f - d * (15.0f + (x * 6.3f)) * (cellSize / 2.0f), 0.0f, 1.0f);
                 snow += newd;
             }
         }
     }
 
-    return image.Sample(builtin_texture_sampler, uv_in) + float4(snow, snow, snow, snow) + random * 0.005f;
+    return image.Sample(builtin_texture_sampler, uv_in) + float4(snow, snow, snow, snow) + random * 0.02f;
 }
